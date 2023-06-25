c2da3f971824d31268ec0d6de914b7dc90a18d78
Chinyere <chinyereunamba17@gmail.com>
2023-06-26 00:25:07 +0100

    Added Mobile Nav

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+const body = document.querySelector("body");
 const decrease = document.getElementById("decrement");
 const increase = document.getElementById("increment");
 const number = document.getElementById("number");
 const addToCart = document.querySelector(".add button");
 const noInCart = document.getElementById("badge");
-const cartContainer = document.querySelector(".cart-content .content");
+const cartContainerContent = document.querySelector(".cart-content .content");
 const productCount = document.getElementById("product-count");
 const price = document.getElementById("product-price");
-
+const cartEmptyP = document.querySelector(".content .empty");
 const total = document.getElementById("total");
+const cartContainer = document.querySelector(".cart-content");
+const cartEl = document.querySelector(".user-info .cart");
+const burger = document.querySelector(".burger");
+const closeEl = document.querySelector(".nav-links span");
+const navMobile = document.querySelector(".nav-links");
 
 if (number.textContent == 0) {
     noInCart.style = `display:none`;
@@ -40,12 +46,29 @@ addToCart.addEventListener("click", () => {
     cart();
 });
 
+cartEl.addEventListener("click", () => {
+    cartContainer.classList.toggle("active");
+    
+});
+
+burger.addEventListener("click", () => {
+    navMobile.classList.add("active");
+});
+
+closeEl.addEventListener("click", () => {
+    navMobile.classList.remove("active");
+});
+
 function cart() {
     if (number.textContent != 0) {
         productCount.textContent = number.textContent;
-        total.textContent = `$${Number(number.textContent) * Number(price.textContent)}`
-        cartContainer.classList.add("active");
+        total.textContent = `$${
+            Number(number.textContent) * Number(price.textContent)
+        }`;
+        cartContainerContent.classList.add("active");
+        cartEmptyP.style = `display:none`;
     } else {
-        cartContainer.classList.remove("active");
+        cartContainerContent.classList.remove("active");
+        cartEmptyP.style = `display:block`;
     }
 }
